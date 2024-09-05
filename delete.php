1ff9b86dae9bb5117dfeb9fbc6b8d40fa77b4263
Thedarkzeros <hy560181@gmail.com>
2024-09-05 23:05:10 +0700

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,21 @@
+ <?php
+  require("connect.php");
+
+ if(isset($_GET['delete_id'])){
+
+        $id = $_GET['delete_id'];
+
+        // sql to delete a record
+        $sql = "DELETE FROM data_student WHERE id= $id";
+
+        if (mysqli_query($con, $sql)) {
+            echo "<br>ลบข้อมูลสำเร็จ</br>";
+
+            echo "<a href='index.php'>กลับหน้าหลัก</a>";
+
+        } else {
+         
+            echo "Error deleting record: " . mysqli_error($conn);
+         }
+      }
+?>
