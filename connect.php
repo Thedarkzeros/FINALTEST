b86d80bb6b4e2183d8549d1bfbdda43c9c8be2ce
Thedarkzeros <hy560181@gmail.com>
2024-09-12 22:37:08 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -12,6 +12,6 @@ $con = new mysqli($host, $username, $password, $dbname);
 if ($con->connect_error) {
     die("Connection failed: " . $con->connnect_error);
 } else {
-    echo "เชื่อมต่อข้อมูลสำเร็จ";
+    echo "เชื่อมต่อฐานข้อมูลสำเร็จ";
 }
 ?>
